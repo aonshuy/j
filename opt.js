fa5ed2b991c0969a06f69f8ee9f439c16ff496a5
aonshuy <aonshuy@foxmail.com>
2023-10-20 17:23:27 +0800

    test: clipboard test

--- opt.js
+++ opt.js
@@ -1,45 +1,47 @@
-function getUrlParams(url) {
-    let urlStr = url.split('?')[1]
-    let obj = {};
-    let paramsArr = urlStr.split('&')
-    for (let i = 0, len = paramsArr.length; i < len; i++) {
-        let arr = paramsArr[i].split('=')
-        obj[arr[0]] = arr[1];
-    }
-    return obj
-}
-
-let _log = (msg) => {
-    let el = document.getElementById("log")
-    el.innerHTML = msg
-}
-
-let dst
-let el = document.createElement('a')
-let paras = getUrlParams(location.href)
-if (paras.t === "wmj")
-    dst = `wameiji://main/${paras.dst}`
-console.log(dst)
-
-
-navigator.permissions.query({
-    name: 'clipboard-write'
-}).then(permissionStatus => {
-    // Will be 'granted', 'denied' or 'prompt':
-
-    _log(permissionStatus.state);
-    if (permissionStatus.state === "granted") {
-        navigator.clipboard.writeText(paras.dst);
-        _log('link copied')
-    } else {
-        // Listen for changes to the permission state
-        permissionStatus.onchange = () => {
-            _log(permissionStatus.state);
+window.onload = () => {
+    function getUrlParams(url) {
+        let urlStr = url.split('?')[1]
+        let obj = {};
+        let paramsArr = urlStr.split('&')
+        for (let i = 0, len = paramsArr.length; i < len; i++) {
+            let arr = paramsArr[i].split('=')
+            obj[arr[0]] = arr[1];
         }
+        return obj
     }
-})
+
+    let _log = (msg) => {
+        let el = document.getElementById("log")
+        el.innerText = msg
+    }
+
+    let dst
+    let el = document.createElement('a')
+    let paras = getUrlParams(location.href)
+    if (paras.t === "wmj")
+        dst = `wameiji://main/${paras.dst}`
+    console.log(dst)
+
+
+    navigator.permissions.query({
+        name: 'clipboard-write'
+    }).then(permissionStatus => {
+        // Will be 'granted', 'denied' or 'prompt':
+
+        _log(permissionStatus.state);
+        if (permissionStatus.state === "granted") {
+            navigator.clipboard.writeText(paras.dst);
+            _log('link copied')
+        } else {
+            // Listen for changes to the permission state
+            permissionStatus.onchange = () => {
+                _log(permissionStatus.state);
+            }
+        }
+    })
 
 
 
-el.href = dst
-el.click()
+    el.href = dst
+    el.click()
+}
