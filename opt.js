5b7233aaf9bed7d7de0f8df4c3022562f355094c
aonshuy <aonshuy@foxmail.com>
2023-10-20 14:48:20 +0800

    test1: try to get url params

---
+++ opt.js
@@ -0,0 +1,21 @@
+function getUrlParams(url) {
+    // 通过 ? 分割获取后面的参数字符串
+    let urlStr = url.split('?')[1]
+    // 创建空对象存储参数
+	let obj = {};
+    // 再通过 & 将每一个参数单独分割出来
+	let paramsArr = urlStr.split('&')
+	for(let i = 0,len = paramsArr.length;i < len;i++){
+        // 再通过 = 将每一个参数分割为 key:value 的形式
+		let arr = paramsArr[i].split('=')
+		obj[arr[0]] = arr[1];
+	}
+	return obj
+}
+let dst
+let paras = getUrlParams(location.href)
+if (paras.t==="wmj")
+    dst = `wameiji://main/${paras.dst}`
+
+console.log(paras.dst)
+//location.href = dst
