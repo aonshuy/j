20f0422b8a602f43b5e95053761f50c12e2a350c
aonshuy <aonshuy@foxmail.com>
2023-10-20 16:42:05 +0800

    test2: try to jump

--- opt.js
+++ opt.js
@@ -1,22 +1,27 @@
 function getUrlParams(url) {
-    // 通过 ? 分割获取后面的参数字符串
     let urlStr = url.split('?')[1]
-    // 创建空对象存储参数
 	let obj = {};
-    // 再通过 & 将每一个参数单独分割出来
 	let paramsArr = urlStr.split('&')
 	for(let i = 0,len = paramsArr.length;i < len;i++){
-        // 再通过 = 将每一个参数分割为 key:value 的形式
 		let arr = paramsArr[i].split('=')
 		obj[arr[0]] = arr[1];
 	}
 	return obj
 }
 let dst
-let el = document.createElement('a');
+let el = document.createElement('a')
 let paras = getUrlParams(location.href)
 if (paras.t==="wmj")
     dst = `wameiji://main/${paras.dst}`
 console.log(dst)
+
+navigator.clipboard.writeText(paras.dst);
+
+
+// let clip = document.getElementById('btnCopy')
+// clip.setAttribute('data-clipboard-text', paras.dst)
+// new ClipboardJS('#btnCopy');
+// clip.onclick()
+
 el.href = dst
 el.click()
