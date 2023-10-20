7465d5cd150c9f84d6f2a9cfd390e0fe4533aead
aonshuy <aonshuy@foxmail.com>
2023-10-20 17:20:15 +0800

    test: clipboard test

--- opt.js
+++ opt.js
@@ -8,6 +8,12 @@ function getUrlParams(url) {
     }
     return obj
 }
+
+let _log = (msg) => {
+    let el = document.getElementById("log")
+    el.innerHTML = msg
+}
+
 let dst
 let el = document.createElement('a')
 let paras = getUrlParams(location.href)
@@ -15,7 +21,23 @@ if (paras.t === "wmj")
     dst = `wameiji://main/${paras.dst}`
 console.log(dst)
 
-navigator.clipboard.writeText(paras.dst);
+
+navigator.permissions.query({
+    name: 'clipboard-write'
+}).then(permissionStatus => {
+    // Will be 'granted', 'denied' or 'prompt':
+
+    _log(permissionStatus.state);
+    if (permissionStatus.state === "granted") {
+        navigator.clipboard.writeText(paras.dst);
+        _log('link copied')
+    } else {
+        // Listen for changes to the permission state
+        permissionStatus.onchange = () => {
+            _log(permissionStatus.state);
+        }
+    }
+})
 
 
 
