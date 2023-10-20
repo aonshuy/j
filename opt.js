ca4dc54b082590e2fe15ca47d8226f93564dcfee
aonshuy <aonshuy@foxmail.com>
2023-10-20 16:59:30 +0800

    test: clipboard test

--- opt.js
+++ opt.js
@@ -1,27 +1,23 @@
 function getUrlParams(url) {
     let urlStr = url.split('?')[1]
-	let obj = {};
-	let paramsArr = urlStr.split('&')
-	for(let i = 0,len = paramsArr.length;i < len;i++){
-		let arr = paramsArr[i].split('=')
-		obj[arr[0]] = arr[1];
-	}
-	return obj
+    let obj = {};
+    let paramsArr = urlStr.split('&')
+    for (let i = 0, len = paramsArr.length; i < len; i++) {
+        let arr = paramsArr[i].split('=')
+        obj[arr[0]] = arr[1];
+    }
+    return obj
 }
 let dst
 let el = document.createElement('a')
 let paras = getUrlParams(location.href)
-if (paras.t==="wmj")
+if (paras.t === "wmj")
     dst = `wameiji://main/${paras.dst}`
 console.log(dst)
 
 navigator.clipboard.writeText(paras.dst);
 
 
-// let clip = document.getElementById('btnCopy')
-// clip.setAttribute('data-clipboard-text', paras.dst)
-// new ClipboardJS('#btnCopy');
-// clip.onclick()
 
 el.href = dst
 el.click()
